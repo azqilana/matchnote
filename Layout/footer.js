abb6f080293da2068677e3215f4ea5e2017660ba
Muhammad Azqilana <mazqulunu@gmail.com>
2026-05-31 19:43:03 +0800

    Remove footer HTML and download button from footer.js
    Removed footer HTML structure and download button link.

--- Layout/footer.js
+++ Layout/footer.js
@@ -4,11 +4,6 @@
 
 export default function () {
   return `
-    <footer>
-  <a href="https://github.com/azqilana/mathnote/releases" 
-   target="_blank"
-   id="downloadBtn" class="download-btn">
       <p id="copyright">&copy; Arjuna Arc</p>
-    </footer>
   `
 }
